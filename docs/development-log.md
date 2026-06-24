f2a96ea843aef5053bd0a691d872802810dde57a
parimipujitha269 <parimipujitha269@gmail.com>
2026-06-24 22:19:58 +0530

    Completed homepage UI with responsive styling

---
+++ docs/development-log.md
@@ -0,0 +1,7 @@
+## Version 0.1
+
+- Created project structure
+- Added README
+- Added requirements
+- Designed UI
+- Built homepage HTML
---
+++ frontend/script.js
@@ -0,0 +1,7 @@
+const generateBtn = document.getElementById("generateBtn");
+
+generateBtn.addEventListener("click", function () {
+
+    alert("Generate button clicked!");
+
+});
