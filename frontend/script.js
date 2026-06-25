ad5b8287afc0c686573c8fb55a9d08044d7333f1
parimipujitha269 <parimipujitha269@gmail.com>
2026-06-25 09:41:23 +0530

    Added loading animation and dynamic question display

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,14 @@
 const resumeInput = document.getElementById("resume");
 const roleSelect = document.getElementById("role");
 const generateBtn = document.getElementById("generateBtn");
+const message = document.getElementById("message");
+const resultSection = document.getElementById("resultSection");
+
+const technicalQuestions = document.getElementById("technicalQuestions");
+
+const hrQuestions = document.getElementById("hrQuestions");
+
+const codingQuestions = document.getElementById("codingQuestions");
 
 generateBtn.addEventListener("click", function () {
 
@@ -18,16 +26,61 @@ message.style.color = "red";
 return;
     }
 
-    if (role === "") {
-        alert("Please select a job role.");
-        return;
-    }
+   if (role === "") {
 
-    if (difficulty === null) {
-        alert("Please choose a difficulty level.");
-        return;
-    }
+    message.textContent = "❌ Please select a job role.";
+    message.style.color = "red";
+    return;
 
-    alert("All inputs are valid!");
+}
+
+if (difficulty === null) {
+
+    message.textContent = "❌ Please choose a difficulty level.";
+    message.style.color = "red";
+    return;
+
+}
+   message.textContent = "📄 Reading your resume...";
+message.style.color = "#2563eb";
+
+
+setTimeout(function () {
+
+    message.textContent = "🧠 Analyzing your skills...";
+
+}, 2000);
+setTimeout(function () {
+
+    message.textContent = "🤖 Generating interview questions...";
+
+}, 4000);
+setTimeout(function () {
+
+    resultSection.style.display = "block";
+
+    technicalQuestions.innerHTML = `
+    <ul>
+        <li>What is a REST API?</li>
+        <li>Explain Stack vs Heap Memory.</li>
+        <li>What is Polymorphism?</li>
+    </ul>
+    `;
+    hrQuestions.innerHTML = `
+<ul>
+    <li>Tell me about yourself.</li>
+    <li>Why should we hire you?</li>
+    <li>What are your strengths?</li>
+</ul>
+`;
+codingQuestions.innerHTML = `
+<ul>
+    <li>Reverse a string.</li>
+    <li>Find the largest element in an array.</li>
+    <li>Check whether a string is a palindrome.</li>
+</ul>
+`;
+
+}, 6000);
 
 });
